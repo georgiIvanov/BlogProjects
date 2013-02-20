3b9ac4760bcaf1d37642bedfda590a5c160b35ce
go6obg92@gmail.com <go6obg92@gmail.com>
2013-02-21 00:20:15 +0200

    Test

---
+++ Snake Game/Snake Game/go6obg.cs
@@ -0,0 +1,39 @@
+﻿using Snake_Game;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+class go6obg
+{
+    private int width, height;
+    private SolidBrush brush;
+    public Rectangle foodRec;
+
+
+    public go6obg(Random randFood)
+    {
+        brush = new SolidBrush(Color.Red);
+
+        width = 10;
+        height = 10;
+        foodRec = new Rectangle(randFood.Next(0, 29) * 10, randFood.Next(0, 29) * 10, width, height);
+    }
+
+    public void FoodLocation(Random randFood, Snake s)
+    {
+        foodRec.X = randFood.Next(0, 29) * 10;
+        foodRec.Y = randFood.Next(0, 29) * 10;
+        for (int i = 0; i < s.SnakeRec.Length; i++)
+        {
+            if (this.foodRec.IntersectsWith(s.SnakeRec[i]))
+            {
+                foodRec.X = randFood.Next(0, 29) * 10;
+                foodRec.Y = randFood.Next(0, 29) * 10;
+                i = 0;
+            }
+        }
+    }
+}
+
